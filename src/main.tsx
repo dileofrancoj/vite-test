4b75455da3e01782ff05a2001d08ad5d54f63ffd
Franco Di Leo <dileo.francoj@gmail.com>
2023-02-10 09:47:04 -0300

    chore: add sentry dsn

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import pkg from "../package.json";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+Sentry.init({
+  dsn: "https://60685eeb02b5445695709486668a3a0d@o4504655782346752.ingest.sentry.io/4504655784509440",
+  integrations: [new BrowserTracing()],
+  environment: "live",
+  release: pkg.version,
+  tracesSampleRate: 1.0,
+});
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
-)
+);
